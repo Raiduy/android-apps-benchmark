8610940541fb074d2ca277125db00553ca98a70f
Raiduy <radu.apsan@gmail.com>
2023-06-08 18:29:44 +0200

    non interval starting code

--- IntervalApps/DisplayTest/app/src/main/java/x/intervalapp/displaytest/MainActivity.kt
+++ IntervalApps/DisplayTest/app/src/main/java/x/intervalapp/displaytest/MainActivity.kt
@@ -1,46 +1,48 @@
 package x.intervalapp.displaytest
 
+import android.media.MediaPlayer.OnPreparedListener
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import x.intervalapp.displaytest.ui.theme.DisplayTestTheme
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DisplayTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        // Set up videoview
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mc = MediaController(this)
+        mc.setAnchorView(videoView)
+        mc.setMediaPlayer(videoView)
+        videoView.setMediaController(mc)
+
+        // Set the path to the video
+        val videoUri: Uri = Uri.parse("android.resource://" + packageName + "/"
+                + R.raw.videofile)
+
+        videoView.setVideoURI(videoUri)
+        videoView.setOnPreparedListener(preparedListener)
+
+        videoView.requestFocus()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DisplayTestTheme {
-        Greeting("Android")
+    // Listen for when the video is prepared
+    var preparedListener = OnPreparedListener { m ->
+        try {
+            if (m.isPlaying) {
+                m.stop()
+                m.release()
+            }
+            m.setVolume(0f, 0f)
+            m.isLooping = false
+            m.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
